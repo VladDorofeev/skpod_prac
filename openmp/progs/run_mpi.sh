c1883560739bb4a1b3e08db9b1d636d631720444
VladDorofeev <328777dv@bk.ru>
2024-12-18 22:12:21 +0300

    rm unnecessary rm xfile

--- openmp/progs/run_mpi.sh
+++ openmp/progs/run_mpi.sh
@@ -1,5 +1,4 @@
 rm *.out
 rm *.err
 mpicc -O0 -o mpi_run -D $1 mpi.c 
-mpisubmit.pl -p $2 --stdout run_stat.out --stderr /dev/null mpi_run
-rm mpi_run
+mpisubmit.pl -p $2 --stdout stat_of_run.out --stderr /dev/null mpi_run
