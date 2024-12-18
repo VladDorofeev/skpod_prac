310e31bc8ae166383d0fd45a561bb5862b4b23b8
VladDorofeev <328777dv@bk.ru>
2024-12-18 22:09:14 +0300

    some fix script

--- openmp/progs/run_mpi.sh
+++ openmp/progs/run_mpi.sh
@@ -1,4 +1,5 @@
 rm *.out
 rm *.err
 mpicc -O0 -o mpi_run -D $1 mpi.c 
-mpisubmit.pl -p $2 --stdout mpi_run.out --stderr /dev/null mpi_run
+mpisubmit.pl -p $2 --stdout run_stat.out --stderr /dev/null mpi_run
+rm mpi_run
